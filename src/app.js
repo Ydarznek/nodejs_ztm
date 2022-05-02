6b48f613564bee263bc051ef241b9efafb679706
Iuliia Darznek <iuliia_darznek@epam.com>
2022-05-02 17:46:44 +0300

    Adds versioning for APIs

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const morgan = require('morgan');
 
 const cors = require('cors');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const api = require('./routes/api');
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(morgan('short'));
 
 app.use(express.json());
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 module.exports = app;
